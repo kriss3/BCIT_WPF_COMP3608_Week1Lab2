f62ca15a54262bc27b2bfe8a712f95dd3ab378bb
kszczurowski <kszczurowski@absolute.com>
2017-10-29 23:14:16 -0700

    Commit of a fully working solution. Application runs and conforms with requiremens. There is a hidden exception that occurs when richTextBox is tryig to use property of fontSlider befor fontSlider is intialized (I think it needs to be initialized in XAML code)

--- BCIT_WPF_COMP3608_Week1Lab2/MainWindow.xaml.cs
+++ BCIT_WPF_COMP3608_Week1Lab2/MainWindow.xaml.cs
@@ -23,6 +23,50 @@ namespace BCIT_WPF_COMP3608_Week1Lab2
         public MainWindow()
         {
             InitializeComponent();
+            foreach (FontFamily f in Fonts.SystemFontFamilies)
+            {
+                ListBoxItem lbi = new ListBoxItem();
+                lbi.Content = f.ToString();
+                lbi.FontFamily = f;
+                listBox1.Items.Add(lbi);
+            }
+
+        }
+
+        private void btnBold_Click(object sender, RoutedEventArgs e)
+        {
+            var selection = richTextBox1.Selection.GetPropertyValue(FontWeightProperty).ToString();
+            if (selection.Equals("Bold"))
+                richTextBox1.Selection.ApplyPropertyValue(FontWeightProperty, FontWeights.Normal);
+            else
+                richTextBox1.Selection.ApplyPropertyValue(FontWeightProperty, FontWeights.Bold);
+        }
+
+        private void btnItalic_Click(object sender, RoutedEventArgs e)
+        {
+            var selection = richTextBox1.Selection.GetPropertyValue(FontStyleProperty).ToString();
+            if (selection.Equals("Italic"))
+                richTextBox1.Selection.ApplyPropertyValue(FontStyleProperty, FontStyles.Normal);
+            else
+                richTextBox1.Selection.ApplyPropertyValue(FontStyleProperty, FontStyles.Italic);
+        }
+
+        private void fontSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            try
+            {
+                richTextBox1.Selection.ApplyPropertyValue(FontSizeProperty, fontSlider.Value.ToString());
+            }
+            catch { }
+            //{
+                //MessageBox.Show(ex.Message,"Error Occured",MessageBoxButton.OK,MessageBoxImage.Error);
+                //throw;
+            //}
+        }
+
+        private void listBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            richTextBox1.Selection.ApplyPropertyValue(FontFamilyProperty, ((ListBoxItem)listBox1.SelectedItem).FontFamily);
         }
     }
 }
