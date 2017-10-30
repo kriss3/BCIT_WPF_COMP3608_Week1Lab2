86be5bc53ee4b85ade088ef5dcdea9741576033f
kszczurowski <kszczurowski@absolute.com>
2017-10-30 00:03:58 -0700

    Minor changes to UI, cleanup code behind, comments

--- BCIT_WPF_COMP3608_Week1Lab2/MainWindow.xaml.cs
+++ BCIT_WPF_COMP3608_Week1Lab2/MainWindow.xaml.cs
@@ -1,22 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BCIT_WPF_COMP3608_Week1Lab2
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
+    /// Krzysztof Szczurowski
+    /// Repo: https://github.com/kriss3/BCIT_WPF_COMP3608_Week1Lab2.git
     /// </summary>
     public partial class MainWindow : Window
     {
@@ -30,7 +21,6 @@ namespace BCIT_WPF_COMP3608_Week1Lab2
                 lbi.FontFamily = f;
                 listBox1.Items.Add(lbi);
             }
-
         }
 
         private void btnBold_Click(object sender, RoutedEventArgs e)
